bfe50cada0bd5f10aecf1ab30940bafef6bc3e27
marcinele <marcinten@gmail.com>
2021-05-06 21:35:33 +0200

    - modified startup arguments

--- src/main/java/com/simpleFTP/Main.java
+++ src/main/java/com/simpleFTP/Main.java
@@ -9,11 +9,18 @@ public class Main {
 
         if(args[0].equals("server"))
         {
-            ConnectionRequestHandler connectionRequestHandler = new ConnectionRequestHandler(10101);
-            connectionRequestHandler.startServer();
+            if(args.length != 3){
+                System.out.println("Wrong arguments. Proper args \" server <serv_ip_addr> <port>\"");
+            }
+            try{
+                ConnectionRequestHandler connectionRequestHandler = new ConnectionRequestHandler(args[1], Integer.parseInt(args[2]));
+                connectionRequestHandler.startServer();
+            } catch (Exception e){
+                System.out.println("Wrong arguments or internal error");
+            }
         }
         else if(args[0].equals("client")){
-            ClientPI clientPI = new ClientPI("localhost", 10101);
+            /*ClientPI clientPI = new ClientPI("localhost", 10101);
             clientPI.connect();
             try{
                 clientPI.login();
@@ -23,6 +30,8 @@ public class Main {
             } catch (Exception e){
                 e.printStackTrace();
             }
+            */
+            System.out.println("Client is not supported yet.");
         }
     }
 }
--- src/main/java/com/simpleFTP/server/ConnectionRequestHandler.java
+++ src/main/java/com/simpleFTP/server/ConnectionRequestHandler.java
@@ -1,24 +1,27 @@
 package com.simpleFTP.server;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ConnectionRequestHandler extends Thread{
     private ServerSocket serverSocket;
+    private String ip_addr;
     private int port;
     private boolean running = false;
 
-    public ConnectionRequestHandler(int port)
-    {
+    public ConnectionRequestHandler(String ip_addr, int port) {
         this.port = port;
+        this.ip_addr = ip_addr;
     }
 
     public void startServer()
     {
         try
         {
-            this.serverSocket = new ServerSocket(this.port);
+            this.serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress(ip_addr, port));
             this.start();
         }
         catch(Exception e)
